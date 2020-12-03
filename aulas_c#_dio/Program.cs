cd6f4e765c1804491a2a2e5e2e94d1da32c53574
jameson souza <jamesonbatista25@gmail.com>
2020-12-03 00:43:35 -0300

    primeiro commit

---
+++ aulas_c#_dio/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace aulas_c__dio
+{
+    class Program
+    {
+        static void Main(string[] args)
+{
+    int numeroDeVezes = 5;
+
+    for(int i = 0; i <numeroDeVezes; i++)
+
+         {
+    Console.WriteLine($"vamos ver agora! {i}");    
+          }
+        }
+    
+    }
+}
